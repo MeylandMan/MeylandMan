57d5063df66cafcd5abee0dba8033d925941c80c
MeylandMan <m.dunooman0@gmail.com>
2025-11-15 20:54:33 +0100

    Structured NavBar html

--- src/sections/NavBar.jsx
+++ src/sections/NavBar.jsx
@@ -2,7 +2,39 @@ const NavBar = () => {
     return (
         <section>
             <nav>
-                <h1>NavBar</h1>
+                Meyland
+            </nav>
+            <nav>
+                <ul>
+                    <li>
+                        About
+                    </li>
+                    <li>
+                        Formation
+                    </li>
+                    <li>
+                        Experience
+                    </li>
+                    <li>
+                        Projects
+                    </li>
+                    <li>
+                        Dev Tools
+                    </li>
+                    <li>
+                        Skills
+                    </li>
+                </ul>
+            </nav>
+            <nav>
+                <ul>
+                    <li>
+                        Get CV
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+                </ul>
             </nav>
         </section>
     )
