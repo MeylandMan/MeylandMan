9e3c1523cdc64a2997ab2ffcc53e3a471cd2349e
MeylandMan <m.dunooman0@gmail.com>
2025-11-15 22:00:43 +0100

    Added experiences const and some formations datas

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,51 @@
+import { text } from "stream/consumers";
+
 export const formations = [
+    {
+        title: "Lycée Marianne",
+        diploma: "DNB - Diplome National du Brevet",
+        date: "Sept. 2020 - Jul. 2021",
+
+        image: "",
+        alt: "Lycée Marianne Logo",
+    },
+    {
+        title: "Lycée Marianne",
+        diploma: "Baccalauréat Scientifique",
+        date: "Sept. 2023 - Jul. 2024",
+
+        image: "",
+        alt: "Lycée Marianne Logo",
+    },
     {
         title: "Collège de Paris",
         diploma: "Bachelor Degree",
-        date: "Oct. 2025 - aujourd'hui",
+        date: "Oct. 2025 - today",
 
         image: "",
         alt: "Collège de Paris Logo",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Freelance Fullstack Developer",
+        status: "Ongoing",
+        date: "Aug. 2025 - today",
+        location: "Île-de-France, France · Remote",
+        description: [
+            {
+                text: "Developing web applications for various clients using modern technologies such as React, Node.js, and Tailwind CSS.",
+            },
+            {
+                text: "Collaborating with clients to gather requirements, provide updates, and ensure satisfaction with the final product.",
+            },
+            {
+                text: "maintaining and updating existing applications to improve performance, fix bugs, and add new features.",
+            },
+            {
+                text: "lead development projects from conception to deployment, ensuring timely delivery and adherence to client specifications.",
+            }
+        ]
     }
 ];
