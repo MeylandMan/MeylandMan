df58261634de979d389a79c12c6768f480c4c30d
MeylandMan <m.dunooman0@gmail.com>
2025-11-15 21:36:03 +0100

    Added About section

---
+++ src/sections/About.jsx
@@ -0,0 +1,26 @@
+const About = () => {
+    return (
+        <section>
+            <div>
+                <h2>About Me</h2>
+                <p>
+                    I give a <strong>HUGE importance on optimizations</strong> and performances. 
+                <br/>
+                    My goal is to deliver <strong>clean</strong>, <strong>efficient</strong> and <strong>scalable</strong> code.
+                <br/>
+                    I'm always eager to <strong>learn new technologies</strong> and improve my skills.
+                </p>
+                <p>
+                    I like everything around computer science, from <strong>web development</strong> to <strong>3D programming</strong> and <strong>AI</strong>.
+                <br/>
+                    I'm also passionate about <strong>video games</strong> and their development.
+                </p>
+            </div>
+            <div>
+                <img src="" alt="Meyland Picture"/>
+            </div>
+        </section>
+    )
+};
+
+export default About;
