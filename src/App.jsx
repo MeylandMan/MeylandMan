00b193e7a3ef5fe3ededffeb404b2f265fcf5a84
MeylandMan <m.dunooman0@gmail.com>
2025-11-15 23:17:27 +0100

    Added Experience section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import NavBar from '@sections/NavBar';
 import Hero from '@sections/Hero';
 import About from '@sections/About';
 import Formation from '@sections/Formation';
+import Experience from '@sections/Experience';
+
+/*
+  <NavBar />
+  <Hero/>
+  <About/>
+  <Formation />
+*/
 
 function App() {
   return (
     <>
-    <NavBar />
-    <Hero/>
-    <About/>
-    <Formation />
+    <Experience />
     </>
   );
 }
