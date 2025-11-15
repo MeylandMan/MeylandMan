d4ef32842fdc1287935638e31c7d5fdbc113fef1
MeylandMan <m.dunooman0@gmail.com>
2025-11-15 21:43:47 +0100

    Added formation constants

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const formations = [
+    {
+        title: "Collège de Paris",
+        diploma: "Bachelor Degree",
+        date: "Oct. 2025 - aujourd'hui",
+
+        image: "",
+        alt: "Collège de Paris Logo",
+    }
+];
