9a036fcc16baa39dbe9cffb418f71cb0ec5ef5e2
MeylandMan <m.dunooman0@gmail.com>
2025-11-15 20:43:53 +0100

    Added NavBar section

---
+++ src/sections/NavBar.jsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return (
+        <section>
+            <nav>
+                <h1>NavBar</h1>
+            </nav>
+        </section>
+    )
+}
+
+export default NavBar;
