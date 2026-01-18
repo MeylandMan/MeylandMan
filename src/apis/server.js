7e1fac200deb433c6792d3d39bf8cac71351e095
MeylandMan <m.dunooman0@gmail.com>
2026-01-18 14:16:47 +0100

    Modified AXIOS_BASE_URL  name

--- src/apis/server.js
+++ src/apis/server.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const fetchData = async (endpoint, params = {}) => {
   try {
-    const route = import.meta.env.AXIOS_BASE_URL || 'http://localhost:8080';
+    const route = import.meta.env.VITE_AXIOS_BASE_URL || 'http://localhost:8080';
     const response = await axios.get(`${route}/api/${endpoint}`, { params });
     return response.data;
   } catch (error) {
